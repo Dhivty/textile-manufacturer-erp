2396e52efcd14166dbc0c28fcdc905ac3d6cf31b
Dhivty <162728125+Dhivty@users.noreply.github.com>
2026-02-16 11:51:05 +0530

    Update server.js
    added the server.js for jobmodule with auth and materials module

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,34 @@ const cors = require("cors");
 require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
-console.log("Auth routes loaded");
+const materialRoutes = require("./routes/materialRoutes");
+const jobRoutes = require("./routes/jobroutes");
+
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/materials", materialRoutes);
+app.use("/api", jobRoutes);
 
+// Test Routes
 app.get("/", (req, res) => {
   res.send("Textile ERP Backend Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
 app.post("/test-register", (req, res) => {
   res.send("Test register works");
 });
+
 app.get("/hello", (req, res) => {
   res.send("HELLO ROUTE WORKS");
 });
-const materialRoutes = require("./routes/materialRoutes");
-app.use("/api/materials", materialRoutes);
+
+// Server
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
